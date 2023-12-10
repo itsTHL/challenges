0eff9321b5759c79eb0011e00672559267488846
Tina Huynh Le <huynhletina@gmail.com>
2023-12-10 21:43:11 +0100

    enable dark mode

--- js/js-basics/js-basics_dark-mode/js/index.js
+++ js/js-basics/js-basics_dark-mode/js/index.js
@@ -4,6 +4,12 @@ const bodyElement = document.querySelector('[data-js="body"]');
 
 const buttonDark = document.querySelector('[data-js="dark-mode-button"]');
 
+buttonDark.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
 const buttonLight = document.querySelector('[data-js="light-mode-button"');
 
 const buttonToggle = document.querySelector('[data-js="toggle-button"');
+
+// bodyElement.classList.remove(".dark");
