4a131df9e15bcc899178d64f4ba5bfe42a704a1b
Tina Huynh Le <huynhletina@gmail.com>
2023-12-10 22:12:25 +0100

    finish challenge

--- js/js-variables-and-numbers/js-variables-and-numbers_tip-calculator/index.js
+++ js/js-variables-and-numbers/js-variables-and-numbers_tip-calculator/index.js
@@ -3,4 +3,8 @@ let tipPercentage = 0.1;
 
 let tipAmount = mealCost * tipPercentage;
 
+console.log(tipPercentage);
+
 let totalCost = mealCost + tipAmount;
+
+console.log("The total cost of your bill is ", totalCost, " euros");
