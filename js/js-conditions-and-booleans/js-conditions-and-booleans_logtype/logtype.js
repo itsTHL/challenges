6892ad45010678d37535ae106fd3146e342bb87d
Tina Huynh Le <huynhletina@gmail.com>
2023-12-12 10:18:39 +0100

    save to work on later

---
+++ js/js-conditions-and-booleans/js-conditions-and-booleans_logtype/logtype.js
@@ -0,0 +1,36 @@
+let data;
+
+switch (typeof data) {
+  case "undefined":
+    console.log("undefined!");
+    break;
+  case "null":
+    console.log("null!");
+    break;
+  case "number":
+    console.log("number!");
+    break;
+  case "!==number":
+    console.log("not a number!");
+    break;
+  case "string":
+    console.log("string!");
+    break;
+  case "boolean":
+    console.log("boolean!");
+    break;
+  case "bigint":
+    console.log("bigint!");
+    break;
+  case "function":
+    console.log("function!");
+    break;
+  case "object":
+    console.log("object!");
+    break;
+  case "number":
+    console.log("array!");
+    break;
+  default:
+    console.log("I have no idea!");
+}
