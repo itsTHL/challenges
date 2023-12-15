6ddc587dbf2995c39c1d66ac25134f30cd48a333
Tina Huynh Le <huynhletina@gmail.com>
2023-12-15 10:18:40 +0100

    create and append new toast message

--- js/js-create-element/js-createelement_toast-messages/js/index.js
+++ js/js-create-element/js-createelement_toast-messages/js/index.js
@@ -4,8 +4,13 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+const newToastMessage = document.createElement("toast-container");
+
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  toastContainer.append(newToastMessage);
+  newToastMessage.classList.add("toast-container__message");
+  newToastMessage.textContent = "I'm a new toast message";
 });
 
 clearButton.addEventListener("click", () => {
