ca844c18db24424e7ca017026db9feb0b0c3eef9
Tina Huynh Le <huynhletina@gmail.com>
2024-01-04 16:13:37 +0100

    import and call fuel function

--- js/js-structure/js-structure_spacerocket/js/launchSequence.js
+++ js/js-structure/js-structure_spacerocket/js/launchSequence.js
@@ -1,4 +1,7 @@
 // Implement the launch sequence function here and export it as the default export.
 import { loadPayload } from "./core/load";
+import { NFSAT } from "./payload/satellites";
+import { FISHSAT } from "./payload/satellites";
 
-loadPayload();
+loadPayload(NFSAT);
+loadPayload(FISHSAT);
