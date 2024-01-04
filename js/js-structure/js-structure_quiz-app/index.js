edee976955cf0dec0a4bbf4c6c6c0f07c9fa5f8a
Tina Huynh Le <huynhletina@gmail.com>
2024-01-04 15:36:38 +0100

    create app.js, move function into new file incl necessary imports

--- js/js-structure/js-structure_quiz-app/index.js
+++ js/js-structure/js-structure_quiz-app/index.js
@@ -1,17 +1,4 @@
-// Store the cards in a global state
-import { cards } from "./global.js";
-import Form from "./components/Form/Form.js";
-import CardList from "./components/CardList/CardList.js";
-import Bookmark from "./components/Bookmark/Bookmark.js";
-import Header from "./components/Header/Header.js";
-
-function App() {
-  const app = document.createElement("main");
-  app.classList.add("app");
-  app.append(Header(), Form(), CardList());
-
-  return app;
-}
+import App from "./components/App/App.js";
 
 // The render function renders the app to the DOM
 function render() {
