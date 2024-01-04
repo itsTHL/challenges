2e4ed066198e3c6c9112bd16ed1e0528b090db26
Tina Huynh Le <huynhletina@gmail.com>
2024-01-04 13:44:43 +0100

    rinse and repeat for pentagon and finish challenge

---
+++ js/js-structure/js-structure_shapes/components/Pentagon/Pentagon.js
@@ -0,0 +1,8 @@
+export default function Pentagon(getRandomColor) {
+  const pentagon = document.createElement("div");
+  pentagon.classList.add("pentagon");
+  pentagon.addEventListener("click", () => {
+    pentagon.style.backgroundColor = getRandomColor();
+  });
+  return pentagon;
+}
--- js/js-structure/js-structure_shapes/index.js
+++ js/js-structure/js-structure_shapes/index.js
@@ -1,6 +1,7 @@
 import { getRandomColor } from "./utils/randomColor.js";
 import Circle from "./components/Circle/Circle.js";
 import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
 
 console.clear();
 
@@ -10,10 +11,6 @@ const circleElement = Circle(getRandomColor);
 
 const squareElement = Square(getRandomColor);
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-});
+const pentagonElement = Pentagon(getRandomColor);
 
-root.append(circleElement, squareElement, pentagon);
+root.append(circleElement, squareElement, pentagonElement);
