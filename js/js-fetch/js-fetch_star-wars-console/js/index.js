1640bec81282bf710d420b5d0bb6fca0d30af052
Tina Huynh Le <huynhletina@gmail.com>
2024-01-08 15:44:22 +0100

    fetch data and console log data

--- js/js-fetch/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,12 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.results);
+  return data;
+}
 
 fetchData();
