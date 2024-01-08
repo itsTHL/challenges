cb0ad7493f4fd1aff5504763e810967d403e30e5
Tina Huynh Le <huynhletina@gmail.com>
2024-01-08 16:13:38 +0100

    implement if condition and try catch

--- js/js-fetch/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -4,11 +4,18 @@ const url = "https://swapi.dev/api/people";
 
 async function fetchData() {
   const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  console.log(data.results);
-  console.log(data.results[2].eye_color);
-  return data;
+
+  if (response.ok) {
+    try {
+      const data = await response.json();
+      console.log(data);
+      console.log(data.results);
+      console.log(data.results[2].eye_color);
+      return data;
+    } catch (error) {
+      console.log("The force is not with you. :(");
+    }
+  }
 }
 
 fetchData();
