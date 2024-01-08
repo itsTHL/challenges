d26baf7703fc1c370de4fa5f64ec1e680b50829c
Tina Huynh Le <huynhletina@gmail.com>
2024-01-08 14:47:24 +0100

    fetch data

--- js/js-fetch/js-fetch_star-wars/js/index.js
+++ js/js-fetch/js-fetch_star-wars/js/index.js
@@ -41,6 +41,8 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  return data;
 }
